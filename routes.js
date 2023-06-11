2fbd4219b04b02eb1ddf011b77cc4a25d1c6b2b0
Arisa Onwuka <onwuka.arisa@gmail.com>
2023-06-11 03:32:02 +0100

    various user files...

---
+++ routes.js
@@ -0,0 +1,47 @@
+const Book = require('./models/book');
+
+module.exports = function(app){
+  app.get('/book', function(req, res){
+    Book.find({}).then(result => {
+      res.json(result);
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while retrieving books');
+    });
+  });
+
+  app.post('/book', function(req, res){
+    const book = new Book({
+      name: req.body.name,
+      isbn: req.body.isbn,
+      author: req.body.author,
+      pages: req.body.pages
+    });
+    book.save().then(result => {
+      res.json({
+        message: "Successfully added book",
+        book: result
+      });
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while saving the book');
+    });
+  });
+
+  app.delete("/book/:isbn", function(req, res){
+    Book.findOneAndRemove(req.query).then(result => {
+      res.json({
+        message: "Successfully deleted the book",
+        book: result
+      });
+    }).catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred while deleting the book');
+    });
+  });
+
+  const path = require('path');
+  app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+};
